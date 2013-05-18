20bca18e179a069151172a3b32782ffbff3910a7
Monty Taylor <mordred@inaugust.com>
2013-05-18 09:02:43 -0700

    Migrate to pbr.
    Fixes bug 1179007.
    
    Change-Id: Ic3b99d88cb9c82112f1e2b51867a0da7fb6ec691

--- setup.py
+++ setup.py
@@ -1,56 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import setuptools
 
-from heatclient.openstack.common import setup
-from heatclient.version import version_info as version
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setuptools.setup(
-    name="python-heatclient",
-    version=version.canonical_version_string(always=True),
-    author='Heat API Developers',
-    author_email='discuss@heat-api.org',
-    description="Client library for Heat orchestration API",
-    long_description=read('README.md'),
-    license='Apache',
-    url='https://github.com/openstack/python-heatclient',
-    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
-    include_package_data=True,
-    install_requires=setup.parse_requirements(),
-    test_suite="nose.collector",
-    cmdclass=setup.get_cmdclass(),
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: OpenStack',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
-    entry_points={
-        'console_scripts': ['heat = heatclient.shell:main']
-    },
-    dependency_links=setup.parse_dependency_links(),
-    tests_require=setup.parse_requirements(['tools/test-requires']),
-    setup_requires=['setuptools-git>=0.4'],
-    data_files=[('heatclient', ['heatclient/versioninfo'])]
-)
+    setup_requires=['d2to1>=0.2.10,<0.3', 'pbr>=0.5.10,<0.6'],
+    d2to1=True)
