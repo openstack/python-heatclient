56984c1589f0e3e80ed6f1442b588669b36d3c7c
Steve Baker <sbaker@redhat.com>
2014-01-06 17:17:58 +1300

    Move template_format from heat to heatclient
    heatclient needs to (YAML) parse the template to allow it to populate
    the content for get_files function calls. For this reason, template_format
    (and environment_format) have been moved to heatclient so this code can be
    shared by both heat and heatclient.
    
    Change-Id: I3cc11cd57256ad539efb4dab314ac07547e9e6a2

---
+++ heatclient/tests/test_environment_format.py
@@ -0,0 +1,69 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from heatclient.common import environment_format
+
+import mock
+import testscenarios
+import testtools
+import yaml
+
+
+load_tests = testscenarios.load_tests_apply_scenarios
+
+
+class YamlEnvironmentTest(testtools.TestCase):
+
+    def test_minimal_yaml(self):
+        yaml1 = ''
+        yaml2 = '''
+parameters: {}
+resource_registry: {}
+'''
+        tpl1 = environment_format.parse(yaml1)
+        environment_format.default_for_missing(tpl1)
+        tpl2 = environment_format.parse(yaml2)
+        self.assertEqual(tpl1, tpl2)
+
+    def test_wrong_sections(self):
+        env = '''
+parameters: {}
+resource_regis: {}
+'''
+        self.assertRaises(ValueError, environment_format.parse, env)
+
+    def test_bad_yaml(self):
+        env = '''
+parameters: }
+'''
+        self.assertRaises(ValueError, environment_format.parse, env)
+
+
+class YamlParseExceptions(testtools.TestCase):
+
+    scenarios = [
+        ('scanner', dict(raised_exception=yaml.scanner.ScannerError())),
+        ('parser', dict(raised_exception=yaml.parser.ParserError())),
+        ('reader',
+         dict(raised_exception=yaml.reader.ReaderError('', '', '', '', ''))),
+    ]
+
+    def test_parse_to_value_exception(self):
+        text = 'not important'
+
+        with mock.patch.object(yaml, 'load') as yaml_loader:
+            yaml_loader.side_effect = self.raised_exception
+
+            self.assertRaises(ValueError,
+                              environment_format.parse, text)
