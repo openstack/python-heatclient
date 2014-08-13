6877ee1234869a59a9d32e47cf691b2b06472208
Winnie Tsang <wtsang@us.ibm.com>
2014-08-13 12:22:22 -0700

    Add stack-update argument for patching parameters
    A new argument named existing for stack-update will be
    supported by the heat engine to use the parameters stored
    in the Db.
    
    Partially-implements: blueprint troubleshooting-low-level-control
    Partial-Bug: 1224828
    Change-Id: I30a2b223d7dd19a717ed989b419b1e6cb2ac143b

--- heatclient/tests/test_shell.py
+++ heatclient/tests/test_shell.py
@@ -1399,6 +1399,87 @@ class ShellTestUserPass(ShellBase):
             self.assertRegexpMatches(update_text, r)
 
     @httpretty.activate
+    def test_stack_update_with_existing_parameters(self):
+        self.register_keystone_auth_fixture()
+        template_file = os.path.join(TEST_VAR_DIR, 'minimal.template')
+        template_data = open(template_file).read()
+        resp = fakes.FakeHTTPResponse(
+            202,
+            'Accepted',
+            {},
+            'The request is accepted for processing.')
+        expected_data = {
+            'files': {},
+            'environment': {},
+            'template': jsonutils.loads(template_data),
+            'parameters': {},
+            'disable_rollback': False}
+        http.HTTPClient.json_request(
+            'PATCH', '/stacks/teststack2/2',
+            data=expected_data,
+            headers={'X-Auth-Key': 'password', 'X-Auth-User': 'username'}
+        ).AndReturn((resp, None))
+        fakes.script_heat_list()
+
+        self.m.ReplayAll()
+
+        update_text = self.shell(
+            'stack-update teststack2/2 '
+            '--template-file=%s '
+            '--enable-rollback '
+            '--existing' % template_file)
+
+        required = [
+            'stack_name',
+            'id',
+            'teststack2',
+            '1'
+        ]
+        for r in required:
+            self.assertRegexpMatches(update_text, r)
+
+    @httpretty.activate
+    def test_stack_update_with_patched_existing_parameters(self):
+        self.register_keystone_auth_fixture()
+        template_file = os.path.join(TEST_VAR_DIR, 'minimal.template')
+        template_data = open(template_file).read()
+        resp = fakes.FakeHTTPResponse(
+            202,
+            'Accepted',
+            {},
+            'The request is accepted for processing.')
+        expected_data = {
+            'files': {},
+            'environment': {},
+            'template': jsonutils.loads(template_data),
+            'parameters': {'"KeyPairName': 'updated_key"'},
+            'disable_rollback': False}
+        http.HTTPClient.json_request(
+            'PATCH', '/stacks/teststack2/2',
+            data=expected_data,
+            headers={'X-Auth-Key': 'password', 'X-Auth-User': 'username'}
+        ).AndReturn((resp, None))
+        fakes.script_heat_list()
+
+        self.m.ReplayAll()
+
+        update_text = self.shell(
+            'stack-update teststack2/2 '
+            '--template-file=%s '
+            '--enable-rollback '
+            '--parameters="KeyPairName=updated_key" '
+            '--existing' % template_file)
+
+        required = [
+            'stack_name',
+            'id',
+            'teststack2',
+            '1'
+        ]
+        for r in required:
+            self.assertRegexpMatches(update_text, r)
+
+    @httpretty.activate
     def test_stack_delete(self):
         self.register_keystone_auth_fixture()
         resp = fakes.FakeHTTPResponse(
