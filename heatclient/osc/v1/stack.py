661ced35b4538789ea989bbc83212763b81c112d
Bryan Jones <jonesbr@us.ibm.com>
2015-11-16 20:14:17 +0000

    OpenstackClient plugin for stack output show
    This change implements the "openstack stack output show" command.
    
    Blueprint: heat-support-python-openstackclient
    
    Change-Id: I9c7538bf25664c46b652b4c9e300272691fb49e7

--- heatclient/osc/v1/stack.py
+++ heatclient/osc/v1/stack.py
@@ -699,3 +699,78 @@ class AbandonStack(format_utils.JsonFormat):
         data = list(six.itervalues(stack))
         columns = list(six.iterkeys(stack))
         return columns, data
+
+
+class OutputShowStack(show.ShowOne):
+    """Show stack output."""
+
+    log = logging.getLogger(__name__ + '.OutputShowStack')
+
+    def get_parser(self, prog_name):
+        parser = super(OutputShowStack, self).get_parser(prog_name)
+        parser.add_argument(
+            'stack',
+            metavar='<NAME or ID>',
+            help=_('Name or ID of stack to query')
+        )
+        parser.add_argument(
+            'output',
+            metavar='<OUTPUT NAME>',
+            nargs='?',
+            default=None,
+            help=_('Name of an output to display')
+        )
+        parser.add_argument(
+            '--all',
+            action='store_true',
+            help=_('Display all stack outputs')
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)', parsed_args)
+
+        client = self.app.client_manager.orchestration
+
+        if not parsed_args.all and parsed_args.output is None:
+            msg = _('Either <OUTPUT NAME> or --all must be specified.')
+            raise exc.CommandError(msg)
+
+        if parsed_args.all and parsed_args.output is not None:
+            msg = _('Cannot specify both <OUTPUT NAME> and --all.')
+            raise exc.CommandError(msg)
+
+        if parsed_args.all:
+            try:
+                stack = client.stacks.get(parsed_args.stack)
+            except heat_exc.HTTPNotFound:
+                msg = _('Stack not found: %s') % parsed_args.stack
+                raise exc.CommandError(msg)
+
+            outputs = stack.to_dict().get('outputs', [])
+            columns = []
+            values = []
+            for output in outputs:
+                columns.append(output['output_key'])
+                values.append(heat_utils.json_formatter(output))
+
+            return columns, values
+
+        try:
+            output = client.stacks.output_show(parsed_args.stack,
+                                               parsed_args.output)['output']
+        except heat_exc.HTTPNotFound:
+            msg = _('Stack %(id)s or output %(out)s not found.') % {
+                'id': parsed_args.stack, 'out': parsed_args.output}
+            raise exc.CommandError(msg)
+
+        if 'output_error' in output:
+            msg = _('Output error: %s') % output['output_error']
+            raise exc.CommandError(msg)
+
+        if (isinstance(output['output_value'], list) or
+                isinstance(output['output_value'], dict)):
+            output['output_value'] = heat_utils.json_formatter(
+                output['output_value'])
+
+        return self.dict2columns(output)
--- heatclient/tests/unit/osc/v1/test_stack.py
+++ heatclient/tests/unit/osc/v1/test_stack.py
@@ -638,3 +638,93 @@ class TestStackAbandon(TestStack):
         parsed_args = self.check_parser(self.cmd, arglist, [])
 
         self.assertRaises(exc.CommandError, self.cmd.take_action, parsed_args)
+
+
+class TestStackOutputShow(TestStack):
+
+    outputs = [
+        {'output_key': 'output1', 'output_value': 'value1'},
+        {'output_key': 'output2', 'output_value': 'value2',
+         'output_error': 'error'}
+    ]
+
+    response = {
+        'outputs': outputs,
+        'stack_name': 'my_stack'
+    }
+
+    def setUp(self):
+        super(TestStackOutputShow, self).setUp()
+        self.cmd = stack.OutputShowStack(self.app, None)
+        self.stack_client.get = mock.MagicMock(
+            return_value=stacks.Stack(None, self.response))
+
+    def test_stack_output_show_no_output(self):
+        arglist = ['my_stack']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        error = self.assertRaises(exc.CommandError,
+                                  self.cmd.take_action, parsed_args)
+        self.assertEqual('Either <OUTPUT NAME> or --all must be specified.',
+                         str(error))
+
+    def test_stack_output_show_output_and_all(self):
+        arglist = ['my_stack', 'output1', '--all']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        error = self.assertRaises(exc.CommandError,
+                                  self.cmd.take_action, parsed_args)
+        self.assertEqual('Cannot specify both <OUTPUT NAME> and --all.',
+                         str(error))
+
+    def test_stack_output_show_all(self):
+        arglist = ['my_stack', '--all']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        columns, outputs = self.cmd.take_action(parsed_args)
+
+        self.stack_client.get.assert_called_with('my_stack')
+        self.assertEqual(['output1', 'output2'], columns)
+
+    def test_stack_output_show_output(self):
+        arglist = ['my_stack', 'output1']
+        self.stack_client.output_show = mock.MagicMock(
+            return_value={'output': self.outputs[0]})
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        columns, outputs = self.cmd.take_action(parsed_args)
+
+        self.stack_client.output_show.assert_called_with('my_stack', 'output1')
+        self.assertEqual(('output_key', 'output_value'), columns)
+        self.assertEqual(('output1', 'value1'), outputs)
+
+    def test_stack_output_show_not_found(self):
+        arglist = ['my_stack', '--all']
+        self.stack_client.get.side_effect = heat_exc.HTTPNotFound
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        error = self.assertRaises(exc.CommandError,
+                                  self.cmd.take_action, parsed_args)
+        self.assertEqual('Stack not found: my_stack', str(error))
+
+    def test_stack_output_show_output_error(self):
+        arglist = ['my_stack', 'output2']
+        self.stack_client.output_show = mock.MagicMock(
+            return_value={'output': self.outputs[1]})
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        error = self.assertRaises(exc.CommandError,
+                                  self.cmd.take_action, parsed_args)
+        self.assertEqual('Output error: error', str(error))
+        self.stack_client.output_show.assert_called_with('my_stack', 'output2')
+
+    def test_stack_output_show_bad_output(self):
+        arglist = ['my_stack', 'output3']
+        self.stack_client.output_show.side_effect = heat_exc.HTTPNotFound
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        error = self.assertRaises(exc.CommandError,
+                                  self.cmd.take_action, parsed_args)
+        self.assertEqual('Stack my_stack or output output3 not found.',
+                         str(error))
+        self.stack_client.output_show.assert_called_with('my_stack', 'output3')
