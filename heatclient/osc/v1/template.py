8601c3097a575fb33f528ee2be16834b632d184c
Bryan Jones <jonesbr@us.ibm.com>
2015-11-24 15:44:36 +0000

    OpenstackClient plugin for template function list
    This change implements the "openstack orchestration template function
    list" command.
    
    Blueprint: heat-support-python-openstackclient
    
    Change-Id: I7565a1cdd7445f77e69e393902a76dadef440d45

--- heatclient/osc/v1/template.py
+++ heatclient/osc/v1/template.py
@@ -17,6 +17,9 @@ import logging
 from cliff import lister
 from openstackclient.common import utils
 
+from heatclient import exc
+from heatclient.openstack.common._i18n import _
+
 
 class VersionList(lister.Lister):
     """List the available template versions."""
@@ -34,4 +37,38 @@ class VersionList(lister.Lister):
         return (
             fields,
             (utils.get_item_properties(s, fields) for s in versions)
+        )
+
+
+class FunctionList(lister.Lister):
+    """List the available functions."""
+
+    log = logging.getLogger(__name__ + '.FunctionList')
+
+    def get_parser(self, prog_name):
+        parser = super(FunctionList, self).get_parser(prog_name)
+        parser.add_argument(
+            'template_version',
+            metavar='<TEMPLATE_VERSION>',
+            help=_('Template version to get the functions for')
+        )
+
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)', parsed_args)
+
+        client = self.app.client_manager.orchestration
+
+        version = parsed_args.template_version
+        try:
+            functions = client.template_versions.get(version)
+        except exc.HTTPNotFound:
+            msg = _('Template version not found: %s') % version
+            raise exc.CommandError(msg)
+
+        fields = ['functions', 'description']
+        return (
+            fields,
+            (utils.get_item_properties(s, fields) for s in functions)
         )
--- heatclient/tests/unit/osc/v1/test_template.py
+++ heatclient/tests/unit/osc/v1/test_template.py
@@ -14,6 +14,7 @@
 
 import mock
 
+from heatclient import exc
 from heatclient.osc.v1 import template
 from heatclient.tests.unit.osc.v1 import fakes
 from heatclient.v1 import template_versions
@@ -47,3 +48,35 @@ class TestTemplateVersionList(TestTemplate):
 
         self.assertEqual(['version', 'type'], columns)
         self.assertEqual([('HOT123', 'hot'), ('CFN456', 'cfn')], list(data))
+
+
+class TestTemplateFunctionList(TestTemplate):
+
+    defaults = [
+        {'functions': 'func1', 'description': 'Function 1'},
+        {'functions': 'func2', 'description': 'Function 2'}
+    ]
+
+    def setUp(self):
+        super(TestTemplateFunctionList, self).setUp()
+        tv1 = template_versions.TemplateVersion(None, self.defaults[0])
+        tv2 = template_versions.TemplateVersion(None, self.defaults[1])
+        self.template_versions.get = mock.MagicMock(return_value=[tv1, tv2])
+        self.cmd = template.FunctionList(self.app, None)
+
+    def test_function_list(self):
+        arglist = ['version1']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        columns, data = self.cmd.take_action(parsed_args)
+
+        self.assertEqual(['functions', 'description'], columns)
+        self.assertEqual([('func1', 'Function 1'), ('func2', 'Function 2')],
+                         list(data))
+
+    def test_function_list_not_found(self):
+        arglist = ['bad_version']
+        self.template_versions.get.side_effect = exc.HTTPNotFound
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        self.assertRaises(exc.CommandError, self.cmd.take_action, parsed_args)
