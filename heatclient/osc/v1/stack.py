270d0178f81ce87109adde62d510c493ac1ba9d4
Bryan Jones <jonesbr@us.ibm.com>
2016-01-13 15:32:59 +0000

    OpenStackClient plugin for stack output list
    This change implements the 'openstack stack output list' command.
    
    Blueprint: heat-support-python-openstackclient
    
    Change-Id: I1ca54c47076c7216106fb1c938107cf9016e968d

--- heatclient/osc/v1/stack.py
+++ heatclient/osc/v1/stack.py
@@ -774,3 +774,36 @@ class OutputShowStack(show.ShowOne):
                 output['output_value'])
 
         return self.dict2columns(output)
+
+
+class OutputListStack(lister.Lister):
+    """List stack outputs."""
+
+    log = logging.getLogger(__name__ + '.OutputListStack')
+
+    def get_parser(self, prog_name):
+        parser = super(OutputListStack, self).get_parser(prog_name)
+        parser.add_argument(
+            'stack',
+            metavar='<NAME or ID>',
+            help=_('Name or ID of stack to query')
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)', parsed_args)
+
+        client = self.app.client_manager.orchestration
+
+        try:
+            outputs = client.stacks.output_list(parsed_args.stack)['outputs']
+        except heat_exc.HTTPNotFound:
+            msg = _('Stack not found: %s') % parsed_args.stack
+            raise exc.CommandError(msg)
+
+        columns = ['output_key', 'description']
+
+        return (
+            columns,
+            (utils.get_dict_properties(s, columns) for s in outputs)
+        )
