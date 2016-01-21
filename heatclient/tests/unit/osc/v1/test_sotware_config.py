daa8abeb229f86377c8c541b499d9aab4b7e81aa
Mark Vanderwiel <vanderwl@us.ibm.com>
2016-01-21 10:19:34 -0600

    Add openstack client software config create
    Add the openstack software config create command.
    orchestration resource type and show
    
    Based from the existing heat commands:
       heat config-create
    
    Change-Id: I2b29653c2780ea1dbf00295aa5684c884013fed5
    Blueprint: heat-support-python-openstackclient

--- heatclient/tests/unit/osc/v1/test_sotware_config.py
+++ heatclient/tests/unit/osc/v1/test_sotware_config.py
@@ -12,6 +12,7 @@
 #
 
 import mock
+import yaml
 
 from openstackclient.common import exceptions as exc
 
@@ -91,3 +92,120 @@ class TestListConfig(TestConfig):
         self.cmd.take_action(parsed_args)
         self.mock_client.software_configs.list.assert_called_with(
             marker='id123')
+
+
+class TestCreateConfig(TestConfig):
+
+    def setUp(self):
+        super(TestCreateConfig, self).setUp()
+        self.cmd = software_config.CreateConfig(self.app, None)
+        self.mock_client.stacks.validate = mock.Mock()
+        self.mock_client.software_configs.create = mock.Mock(
+            return_value=software_configs.SoftwareConfig(None, {}))
+
+    def test_config_create(self):
+        properties = {
+            'config': '',
+            'group': 'Heat::Ungrouped',
+            'name': 'test',
+            'options': {},
+            'inputs': [],
+            'outputs': []
+        }
+        arglist = ['test']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+        columns, rows = self.cmd.take_action(parsed_args)
+        self.mock_client.stacks.validate.assert_called_with(**{
+            'template': {
+                'heat_template_version': '2013-05-23',
+                'resources': {
+                    'test': {
+                        'type': 'OS::Heat::SoftwareConfig',
+                        'properties': properties}}}})
+        self.mock_client.software_configs.create.assert_called_with(
+            **properties)
+
+    def test_config_create_group(self):
+        properties = {
+            'config': '',
+            'group': 'group',
+            'name': 'test',
+            'options': {},
+            'inputs': [],
+            'outputs': []
+        }
+        arglist = ['test', '--group', 'group']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+        columns, rows = self.cmd.take_action(parsed_args)
+        self.mock_client.stacks.validate.assert_called_with(**{
+            'template': {
+                'heat_template_version': '2013-05-23',
+                'resources': {
+                    'test': {
+                        'type': 'OS::Heat::SoftwareConfig',
+                        'properties': properties}}}})
+        self.mock_client.software_configs.create.assert_called_with(
+            **properties)
+
+    @mock.patch('six.moves.urllib.request.urlopen')
+    def test_config_create_config_file(self, urlopen):
+        properties = {
+            'config': 'config',
+            'group': 'Heat::Ungrouped',
+            'name': 'test',
+            'options': {},
+            'inputs': [],
+            'outputs': []
+        }
+        data = mock.Mock()
+        data.read.side_effect = ['config']
+        urlopen.return_value = data
+
+        arglist = ['test', '--config-file', 'config_file']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+        columns, rows = self.cmd.take_action(parsed_args)
+        self.mock_client.stacks.validate.assert_called_with(**{
+            'template': {
+                'heat_template_version': '2013-05-23',
+                'resources': {
+                    'test': {
+                        'type': 'OS::Heat::SoftwareConfig',
+                        'properties': properties}}}})
+        self.mock_client.software_configs.create.assert_called_with(
+            **properties)
+
+    @mock.patch('six.moves.urllib.request.urlopen')
+    def test_config_create_definition_file(self, urlopen):
+        definition = {
+            'inputs': [
+                {'name': 'input'},
+            ],
+            'outputs': [
+                {'name': 'output'}
+            ],
+            'options': {'option': 'value'}
+        }
+
+        properties = {
+            'config': '',
+            'group': 'Heat::Ungrouped',
+            'name': 'test'
+        }
+        properties.update(definition)
+
+        data = mock.Mock()
+        data.read.side_effect = [yaml.safe_dump(definition)]
+        urlopen.return_value = data
+
+        arglist = ['test', '--definition-file', 'definition-file']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+        columns, rows = self.cmd.take_action(parsed_args)
+        self.mock_client.stacks.validate.assert_called_with(**{
+            'template': {
+                'heat_template_version': '2013-05-23',
+                'resources': {
+                    'test': {
+                        'type': 'OS::Heat::SoftwareConfig',
+                        'properties': properties}}}})
+        self.mock_client.software_configs.create.assert_called_with(
+            **properties)
