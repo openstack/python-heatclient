1970d2a32eba7eb530e3a76b12305c7676bb1cee
Steve Baker <sbaker@redhat.com>
2016-02-03 14:31:46 +1300

    OpenstackClient plugin for stack abandon
    This change implements the "openstack stack abandon" command
    
    Blueprint: heat-support-python-openstackclient
    
    Change-Id: I64a3f0848e9d355b4fdbc7d63e8f6dca762de00e

--- heatclient/osc/v1/stack.py
+++ heatclient/osc/v1/stack.py
@@ -20,8 +20,11 @@ from cliff import show
 from openstackclient.common import exceptions as exc
 from openstackclient.common import parseractions
 from openstackclient.common import utils
+from oslo_serialization import jsonutils
+import six
 from six.moves.urllib import request
 
+from heatclient.common import format_utils
 from heatclient.common import http
 from heatclient.common import template_utils
 from heatclient.common import utils as heat_utils
@@ -652,3 +655,47 @@ class AdoptStack(show.ShowOne):
                 raise exc.CommandError(msg)
 
         return _show_stack(client, stack['id'], format='table', short=True)
+
+
+class AbandonStack(format_utils.JsonFormat):
+    """Abandon stack and output results."""
+
+    log = logging.getLogger(__name__ + '.AbandonStack')
+
+    def get_parser(self, prog_name):
+        parser = super(AbandonStack, self).get_parser(prog_name)
+        parser.add_argument(
+            'stack',
+            metavar='<NAME or ID>',
+            help=_('Name or ID of stack to abandon')
+        )
+        parser.add_argument(
+            '--output-file',
+            metavar='<FILE>',
+            help=_('File to output abandon results')
+        )
+
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)', parsed_args)
+
+        client = self.app.client_manager.orchestration
+
+        try:
+            stack = client.stacks.abandon(stack_id=parsed_args.stack)
+        except heat_exc.HTTPNotFound:
+            msg = _('Stack not found: %s') % parsed_args.stack
+            raise exc.CommandError(msg)
+
+        if parsed_args.output_file is not None:
+            try:
+                with open(parsed_args.output_file, 'w') as f:
+                    f.write(jsonutils.dumps(stack, indent=2))
+                    return [], None
+            except IOError as e:
+                raise exc.CommandError(str(e))
+
+        data = list(six.itervalues(stack))
+        columns = list(six.iterkeys(stack))
+        return columns, data
--- heatclient/tests/unit/osc/v1/test_stack.py
+++ heatclient/tests/unit/osc/v1/test_stack.py
@@ -12,6 +12,7 @@
 #
 
 import copy
+import io
 import mock
 import six
 import testscenarios
@@ -19,6 +20,7 @@ import testscenarios
 from openstackclient.common import exceptions as exc
 from openstackclient.common import utils
 
+from heatclient import exc as heat_exc
 from heatclient.osc.v1 import stack
 from heatclient.tests.unit.osc.v1 import fakes as orchestration_fakes
 from heatclient.v1 import stacks
@@ -582,6 +584,57 @@ class TestStackAdopt(TestStack):
         arglist = ['my_stack', '--adopt-file', self.adopt_file, '--wait']
         self.stack_client.get = mock.MagicMock(return_value=(
             stacks.Stack(None, {'stack_status': 'CREATE_FAILED'})))
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        self.assertRaises(exc.CommandError, self.cmd.take_action, parsed_args)
+
+
+class TestStackAbandon(TestStack):
+
+    columns = ['stack_name', 'stack_status', 'id']
+    data = ['my_stack', 'ABANDONED', '1234']
+
+    response = dict(zip(columns, data))
+
+    def setUp(self):
+        super(TestStackAbandon, self).setUp()
+        self.cmd = stack.AbandonStack(self.app, None)
+        self.stack_client.abandon = mock.MagicMock(return_value=self.response)
+
+    def test_stack_abandon(self):
+        arglist = ['my_stack']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        columns, data = self.cmd.take_action(parsed_args)
+
+        for column in self.columns:
+            self.assertIn(column, columns)
+        for datum in self.data:
+            self.assertIn(datum, data)
+
+    def test_stack_abandon_not_found(self):
+        arglist = ['my_stack']
+        self.stack_client.abandon.side_effect = heat_exc.HTTPNotFound
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        self.assertRaises(exc.CommandError, self.cmd.take_action, parsed_args)
+
+    @mock.patch('heatclient.osc.v1.stack.open', create=True)
+    def test_stack_abandon_output_file(self, mock_open):
+        arglist = ['my_stack', '--output-file', 'file.json']
+        mock_open.return_value = mock.MagicMock(spec=io.IOBase)
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        columns, data = self.cmd.take_action(parsed_args)
+
+        mock_open.assert_called_once_with('file.json', 'w')
+        self.assertEqual([], columns)
+        self.assertIsNone(data)
+
+    @mock.patch('heatclient.osc.v1.stack.open', create=True,
+                side_effect=IOError)
+    def test_stack_abandon_output_file_error(self, mock_open):
+        arglist = ['my_stack', '--output-file', 'file.json']
         parsed_args = self.check_parser(self.cmd, arglist, [])
 
         self.assertRaises(exc.CommandError, self.cmd.take_action, parsed_args)
