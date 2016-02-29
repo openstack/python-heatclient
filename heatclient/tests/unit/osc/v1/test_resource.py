ee73d343f5d125819756a46fa9bb15960a14e1f9
dixiaoli <dixiaobj@cn.ibm.com>
2016-02-29 17:05:40 +0800

    Add OSC plugin for stack resource mark unhealthy
    This change implements the "openstack stack resource mark unhealthy" command
      Based on the existing heat command: heat resource-mark-unhealthy
    
    Change-Id: I8b4735085e1656f991b648d094f690b8d7df451d
    Blueprint: heat-support-python-openstackclient

--- heatclient/tests/unit/osc/v1/test_resource.py
+++ heatclient/tests/unit/osc/v1/test_resource.py
@@ -322,3 +322,43 @@ class TestResourceSignal(TestResource):
             'stack_id': 'my_stack',
             'resource_name': 'my_resource'
         })
+
+
+class TestResourceMarkUnhealthy(TestResource):
+    def setUp(self):
+        super(TestResourceMarkUnhealthy, self).setUp()
+        self.cmd = resource.ResourceMarkUnhealthy(self.app, None)
+        self.resource_client.mark_unhealthy = mock.Mock()
+
+    def test_resource_mark_unhealthy(self):
+        arglist = ['my_stack', 'my_resource', 'reason']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+        self.cmd.take_action(parsed_args)
+        self.resource_client.mark_unhealthy.assert_called_with(**{
+            "stack_id": "my_stack",
+            "resource_name": "my_resource",
+            "mark_unhealthy": True,
+            "resource_status_reason": "reason"
+        })
+
+    def test_resource_mark_unhealthy_reset(self):
+        arglist = ['my_stack', 'my_resource', '--reset']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+        self.cmd.take_action(parsed_args)
+        self.resource_client.mark_unhealthy.assert_called_with(**{
+            "stack_id": "my_stack",
+            "resource_name": "my_resource",
+            "mark_unhealthy": False,
+            "resource_status_reason": ""
+        })
+
+    def test_resource_mark_unhealthy_not_found(self):
+        arglist = ['my_stack', 'my_resource', '--reset']
+        self.resource_client.mark_unhealthy.side_effect = (
+            heat_exc.HTTPNotFound)
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+
+        error = self.assertRaises(exc.CommandError,
+                                  self.cmd.take_action, parsed_args)
+        self.assertEqual('Stack or resource not found: my_stack my_resource',
+                         str(error))
