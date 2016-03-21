openstack/python-heatclient

commit 7627e1bae53747e1471caf632dfc1b6edf2e26cf

Changes to support server-side environment resolution
Adds support in template_utils to optionally produce an ordered list of
environment file names and include the environment file contents in the
files dict. This will be passed to the server to trigger the server-side
handling of resolving overrides for multiple environments.

The original behavior of merging the environments client-side is left in
for backward compatibility purposes. Both are sent to the server. For
mitaka and newer, the client-submitted environment is ignored and the
merging is done server-side. For liberty and older, the
environment_files list is ignored entirely and the passed in environment
is used.

Note that the merging, regardless if it is done client or server-side,
is handled by the same code; it has not been copied or duplicated in the
server.

Change-Id: If187c8ca3d80008c21e8f6066d335ab0c30315e6
Implements: blueprint multi-environments
Depends-On: Ibe46fd35de7988920c101a83259c06c8f8a3ed0b

heatclient/common/template_utils.py
@@ -189,17 +189,51 @@ def deep_update(old, new):
 def process_multiple_environments_and_files(env_paths=None, template=None,
                                             template_url=None,
                                             env_path_is_object=None,
-                                            object_request=None):
+                                            object_request=None,
+                                            env_list_tracker=None):
+    """Reads one or more environment files.
 
+    Reads in each specified environment file and returns a dictionary
+    of the filenames->contents (suitable for the files dict)
+    and the consolidated environment (after having applied the correct
+    overrides based on order).
+
+    If a list is provided in the env_list_tracker parameter, the behavior
+    is altered to take advantage of server-side environment resolution.
+    Specifically, this means:
+
+    * Populating env_list_tracker with an ordered list of environment file
+      URLs to be passed to the server
+    * Including the contents of each environment file in the returned
+      files dict, keyed by one of the URLs in env_list_tracker
+
+    :param env_paths: list of paths to the environment files to load; if
+           None, empty results will be returned
+    :type  env_paths: list or None
+    :param template: unused; only included for API compatibility
+    :param template_url: unused; only included for API compatibility
+    :param env_list_tracker: if specified, environment filenames will be
+           stored within
+    :type  env_list_tracker: list or None
+    :return: tuple of files dict and a dict of the consolidated environment
+    :rtype:  tuple
+    """
     merged_files = {}
     merged_env = {}
 
+    # If we're keeping a list of environment files separately, include the
+    # contents of the files in the files dict
+    include_env_in_files = env_list_tracker is not None
+
     if env_paths:
         for env_path in env_paths:
-            files, env = process_environment_and_files(env_path, template,
-                                                       template_url,
-                                                       env_path_is_object,
-                                                       object_request)
+            files, env = process_environment_and_files(
+                env_path=env_path,
+                template=template,
+                template_url=template_url,
+                env_path_is_object=env_path_is_object,
+                object_request=object_request,
+                include_env_in_files=include_env_in_files)
 
             # 'files' looks like {"filename1": contents, "filename2": contents}
             # so a simple update is enough for merging
@@ -209,12 +243,32 @@ def process_multiple_environments_and_files(env_paths=None, template=None,
             # not enough
             merged_env = deep_update(merged_env, env)
 
+            if env_list_tracker is not None:
+                env_url = utils.normalise_file_path_to_url(env_path)
+                env_list_tracker.append(env_url)
+
     return merged_files, merged_env
 
 
-def process_environment_and_files(env_path=None, template=None,
-                                  template_url=None, env_path_is_object=None,
-                                  object_request=None):
+def process_environment_and_files(env_path=None,
+                                  template=None,
+                                  template_url=None,
+                                  env_path_is_object=None,
+                                  object_request=None,
+                                  include_env_in_files=False):
+    """Loads a single environment file.
+
+    Returns an entry suitable for the files dict which maps the environment
+    filename to its contents.
+
+    :param env_path: full path to the file to load
+    :type  env_path: str or None
+    :param include_env_in_files: if specified, the raw environment file itself
+           will be included in the returned files dict
+    :type  include_env_in_files: bool
+    :return: tuple of files dict and the loaded environment as a dict
+    :rtype:  (dict, dict)
+    """
     files = {}
     env = {}
 
@@ -234,6 +288,10 @@ def process_environment_and_files(env_path=None, template=None,
         env_url = utils.normalise_file_path_to_url(env_path)
         env_base_url = utils.base_url_for_url(env_url)
         raw_env = request.urlopen(env_url).read()
+
+        if include_env_in_files:
+            files[env_url] = raw_env
+
         env = environment_format.parse(raw_env)
 
         resolve_environment_urls(
@@ -246,6 +304,15 @@ def process_environment_and_files(env_path=None, template=None,
 
 def resolve_environment_urls(resource_registry, files, env_base_url,
                              is_object=False, object_request=None):
+    """Handles any resource URLs specified in an environment.
+
+    :param resource_registry: mapping of type name to template filename
+    :type  resource_registry: dict
+    :param files: dict to store loaded file contents into
+    :type  files: dict
+    :param env_base_url: base URL to look in when loading files
+    :type  env_base_url: str or None
+    """
     if resource_registry is None:
         return
 

heatclient/tests/unit/test_template_utils.py
@@ -352,6 +352,42 @@ class ShellEnvironmentTest(testtools.TestCase):
         self.assertEqual(self.template_a.decode('utf-8'),
                          files['http://no.where/path/to/b/a.yaml'])
 
+    def test_process_multiple_environments_and_files_tracker(self):
+        # Setup
+        self.m.StubOutWithMock(request, 'urlopen')
+        env_file1 = '/home/my/dir/env1.yaml'
+
+        env1 = b'''
+        parameters:
+          "param1": "value1"
+        resource_registry:
+          "OS::Thingy1": "file:///home/b/a.yaml"
+        '''
+        request.urlopen('file://%s' % env_file1).AndReturn(
+            six.BytesIO(env1))
+        request.urlopen('file:///home/b/a.yaml').AndReturn(
+            six.BytesIO(self.template_a))
+        request.urlopen('file:///home/b/a.yaml').AndReturn(
+            six.BytesIO(self.template_a))
+        self.m.ReplayAll()
+
+        # Test
+        env_file_list = []
+        files, env = template_utils.process_multiple_environments_and_files(
+            [env_file1], env_list_tracker=env_file_list)
+
+        # Verify
+        expected_env = {'parameters': {'param1': 'value1'},
+                        'resource_registry':
+                            {'OS::Thingy1': 'file:///home/b/a.yaml'}
+                        }
+        self.assertEqual(expected_env, env)
+
+        self.assertEqual(self.template_a.decode('utf-8'),
+                         files['file:///home/b/a.yaml'])
+
+        self.assertEqual(['file:///home/my/dir/env1.yaml'], env_file_list)
+
     def test_global_files(self):
         url = 'file:///home/b/a.yaml'
         env = '''

heatclient/v1/shell.py
@@ -105,8 +105,9 @@ def do_stack_create(hc, args):
         args.template_url,
         args.template_object,
         _authenticated_fetcher(hc))
+    env_files_list = []
     env_files, env = template_utils.process_multiple_environments_and_files(
-        env_paths=args.environment_file)
+        env_paths=args.environment_file, env_list_tracker=env_files_list)
 
     if args.create_timeout:
         logger.warning(_LW('%(arg1)s is deprecated, '
@@ -129,6 +130,10 @@ def do_stack_create(hc, args):
         'files': dict(list(tpl_files.items()) + list(env_files.items())),
         'environment': env
     }
+
+    # If one or more environments is found, pass the listing to the server
+    if env_files_list:
+        fields['environment_files'] = env_files_list
 
     if args.tags:
         fields['tags'] = args.tags
@@ -242,8 +247,9 @@ def do_stack_preview(hc, args):
         args.template_url,
         args.template_object,
         _authenticated_fetcher(hc))
+    env_files_list = []
     env_files, env = template_utils.process_multiple_environments_and_files(
-        env_paths=args.environment_file)
+        env_paths=args.environment_file, env_list_tracker=env_files_list)
 
     fields = {
         'stack_name': args.name,
@@ -257,6 +263,10 @@ def do_stack_preview(hc, args):
         'files': dict(list(tpl_files.items()) + list(env_files.items())),
         'environment': env
     }
+
+    # If one or more environments is found, pass the listing to the server
+    if env_files_list:
+        fields['environment_files'] = env_files_list
 
     if args.tags:
         fields['tags'] = args.tags
@@ -454,9 +464,9 @@ def do_stack_update(hc, args):
         args.template_object,
         _authenticated_fetcher(hc),
         existing=args.existing)
-
+    env_files_list = []
     env_files, env = template_utils.process_multiple_environments_and_files(
-        env_paths=args.environment_file)
+        env_paths=args.environment_file, env_list_tracker=env_files_list)
 
     if args.pre_update:
         template_utils.hooks_to_env(env, args.pre_update, 'pre-update')
@@ -472,6 +482,10 @@ def do_stack_update(hc, args):
         'files': dict(list(tpl_files.items()) + list(env_files.items())),
         'environment': env
     }
+
+    # If one or more environments is found, pass the listing to the server
+    if env_files_list:
+        fields['environment_files'] = env_files_list
 
     if args.tags:
         fields['tags'] = args.tags
@@ -820,8 +834,10 @@ def do_template_validate(hc, args):
         args.template_object,
         _authenticated_fetcher(hc))
 
+    env_files_list = []
     env_files, env = template_utils.process_multiple_environments_and_files(
-        env_paths=args.environment_file)
+        env_paths=args.environment_file, env_list_tracker=env_files_list)
+
     fields = {
         'template': template,
         'parameters': utils.format_parameters(args.parameters),
@@ -831,6 +847,10 @@ def do_template_validate(hc, args):
 
     if args.ignore_errors:
         fields['ignore_errors'] = args.ignore_errors
+
+    # If one or more environments is found, pass the listing to the server
+    if env_files_list:
+        fields['environment_files'] = env_files_list
 
     if args.show_nested:
         fields['show_nested'] = args.show_nested

commit f13d36ae0114e159663ea2a3fa07260df9d9407d

Fix regression handling relative paths in environment
7627e1bae53747e1471caf632dfc1b6edf2e26cf introduced new code to add the
environment to the files mapping, so server side resolution of multiple
environments is possible.

However, the environment added is wrong when you're handling those with
relative paths inside, we must add the resolved version or the keys
don't map to keys in the files map, an you get an error when we try
to locate the file on the server.

Evidently we lack adequate functional test coverage of this, which can
be addressed via a heat patch, but I added a unit test illustrating
the problem.

Change-Id: I3bd2cd48623e4ad1b4067f4cefc7ddff30b9e88d
Closes-Bug: #1560106

heatclient/common/template_utils.py
@@ -289,15 +289,15 @@ def process_environment_and_files(env_path=None,
         env_base_url = utils.base_url_for_url(env_url)
         raw_env = request.urlopen(env_url).read()
 
-        if include_env_in_files:
-            files[env_url] = raw_env
-
         env = environment_format.parse(raw_env)
 
         resolve_environment_urls(
             env.get('resource_registry'),
             files,
             env_base_url)
+
+        if include_env_in_files:
+            files[env_url] = jsonutils.dumps(env)
 
     return files, env
 

heatclient/tests/unit/test_template_utils.py
@@ -409,6 +409,48 @@ class ShellEnvironmentTest(testtools.TestCase):
                          files['file:///home/b/a.yaml'])
 
         self.assertEqual(['file:///home/my/dir/env1.yaml'], env_file_list)
+        self.assertEqual(json.dumps(expected_env),
+                         files['file:///home/my/dir/env1.yaml'])
+
+    def test_process_environment_relative_file_tracker(self):
+
+        self.m.StubOutWithMock(request, 'urlopen')
+        env_file = '/home/my/dir/env.yaml'
+        env_url = 'file:///home/my/dir/env.yaml'
+        env = b'''
+        resource_registry:
+          "OS::Thingy": a.yaml
+        '''
+
+        request.urlopen(env_url).AndReturn(
+            six.BytesIO(env))
+        request.urlopen('file:///home/my/dir/a.yaml').AndReturn(
+            six.BytesIO(self.template_a))
+        request.urlopen('file:///home/my/dir/a.yaml').AndReturn(
+            six.BytesIO(self.template_a))
+        self.m.ReplayAll()
+
+        self.assertEqual(
+            env_url,
+            utils.normalise_file_path_to_url(env_file))
+        self.assertEqual(
+            'file:///home/my/dir',
+            utils.base_url_for_url(env_url))
+
+        env_file_list = []
+        files, env = template_utils.process_multiple_environments_and_files(
+            [env_file], env_list_tracker=env_file_list)
+
+        # Verify
+        expected_env = {'resource_registry':
+                        {'OS::Thingy': 'file:///home/my/dir/a.yaml'}}
+        self.assertEqual(expected_env, env)
+
+        self.assertEqual(self.template_a.decode('utf-8'),
+                         files['file:///home/my/dir/a.yaml'])
+        self.assertEqual(['file:///home/my/dir/env.yaml'], env_file_list)
+        self.assertEqual(json.dumps(expected_env),
+                         files['file:///home/my/dir/env.yaml'])
 
     def test_global_files(self):
         url = 'file:///home/b/a.yaml'