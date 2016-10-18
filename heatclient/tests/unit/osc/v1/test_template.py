0ac4a80dada9a7fb9ee13d965c496b2b7341a4d5
huangtianhua <huangtianhua@huawei.com>
2016-10-18 10:32:01 +0800

    Return condition functions based on the filter param
    Add 'with_condition_func' filter param for API
    template-function-list, if the param set to true,
    the response will include the condition functions.
    
    Change-Id: Ia7b4da71ed5c3da105dd23917e4921a96e445026
    Closes-Bug: #1625505

--- heatclient/tests/unit/osc/v1/test_template.py
+++ heatclient/tests/unit/osc/v1/test_template.py
@@ -54,24 +54,40 @@ class TestTemplateFunctionList(TestTemplate):
 
     defaults = [
         {'functions': 'func1', 'description': 'Function 1'},
-        {'functions': 'func2', 'description': 'Function 2'}
+        {'functions': 'func2', 'description': 'Function 2'},
+        {'functions': 'condition func', 'description': 'Condition Function'}
     ]
 
     def setUp(self):
         super(TestTemplateFunctionList, self).setUp()
-        tv1 = template_versions.TemplateVersion(None, self.defaults[0])
-        tv2 = template_versions.TemplateVersion(None, self.defaults[1])
-        self.template_versions.get.return_value = [tv1, tv2]
+        self.tv1 = template_versions.TemplateVersion(None, self.defaults[0])
+        self.tv2 = template_versions.TemplateVersion(None, self.defaults[1])
+        self.tv_with_cf = template_versions.TemplateVersion(
+            None, self.defaults[2])
+
         self.cmd = template.FunctionList(self.app, None)
 
     def test_function_list(self):
         arglist = ['version1']
         parsed_args = self.check_parser(self.cmd, arglist, [])
-
+        self.template_versions.get.return_value = [self.tv1, self.tv2]
         columns, data = self.cmd.take_action(parsed_args)
 
         self.assertEqual(['functions', 'description'], columns)
         self.assertEqual([('func1', 'Function 1'), ('func2', 'Function 2')],
+                         list(data))
+
+    def test_function_list_with_condition_func(self):
+        arglist = ['version1', '--with_conditions']
+        parsed_args = self.check_parser(self.cmd, arglist, [])
+        self.template_versions.get.return_value = [self.tv1, self.tv2,
+                                                   self.tv_with_cf]
+        columns, data = self.cmd.take_action(parsed_args)
+
+        self.assertEqual(['functions', 'description'], columns)
+        self.assertEqual([('func1', 'Function 1'),
+                          ('func2', 'Function 2'),
+                          ('condition func', 'Condition Function')],
                          list(data))
 
     def test_function_list_not_found(self):
